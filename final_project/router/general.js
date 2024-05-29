f6f043bb552695906634cc511857767d24bf098c
jtoshiro <joshiro@telus.net>
2024-05-29 02:21:03 -0400

    more changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,20 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!isValid(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
+  });
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -41,7 +52,9 @@ public_users.get('/author/:author',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]["reviews"]);
+  
 });
 
 module.exports.general = public_users;
