22c18986c09e4f5b064c48f9b8fd7dcbf54e7e18
jtoshiro <joshiro@telus.net>
2024-05-29 18:25:52 -0400

    final changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,31 +23,42 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({books},null,4));
+    const taskTenPromise = new Promise((resolve, reject)=>{
+        resolve(res.send(JSON.stringify({books},null,4)));
+    });
+    taskTenPromise.then(()=> console.log("Task 10 promise resolved"));
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+    const taskElevenPromise = new Promise((resolve, reject)=>{
         const isbn = req.params.isbn;
-        res.send(books[isbn])
+        resolve(res.send(books[isbn]));
+    });
+    taskElevenPromise.then(()=> console.log("Task 11 promise resolved"));
 });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    const taskTwelvePromise = new Promise((resolve, reject)=>{
         const author = req.params.author;
         let filtered_author = Object.values(books).filter((book) => book.author === author);
-        res.send(filtered_author);
+        resolve(res.send(filtered_author));
     });
+    taskTwelvePromise.then(()=> console.log("Task 12 promise resolved"));
+});
 
 
 // Get all books based on title
-//public_users.get('/title/:title',function (req, res) {
-
-    public_users.get('/title/:title',(req,res)=>{
+public_users.get('/title/:title',(req,res)=>{
+    const taskThirteenPromise = new Promise((resolve, reject)=>{
         const title = req.params.title;
         let filtered_title = Object.values(books).filter((book) => book.title === title);
-        res.send(filtered_title);
+        resolve(res.send(filtered_title));
     });
+    taskThirteenPromise.then(()=> console.log("Task 13 promise resolved"));
+});
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
